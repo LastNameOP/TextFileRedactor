1d3d14b6f761ec09af2019c1b19730b5936d644e
LastHame <cotyuhov@gmail.com>
2022-11-08 23:53:39 +0300

    v1

---
+++ TextChange/TextRedactor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextChange
+{
+    internal class TextRedactor
+    {
+        string dir;
+        public TextRedactor(string dir)
+        {
+                this.dir = dir;
+        }
+
+        public void Write(string message)
+        {
+            StreamWriter textFileWriter = new StreamWriter(dir, true);
+            textFileWriter.WriteLine(message);
+            textFileWriter.Close();
+        }
+        public string Read(int line)
+        {
+            StreamReader textFileReader = new StreamReader(dir);
+            string message = "";
+            for (int i = 0; i < line; i++)
+            {
+                message = textFileReader.ReadLine();
+            }
+            textFileReader.Close(); 
+            return message;
+        }
+
+        public string ReadAll()
+        {
+            StreamReader textFileReader = new StreamReader(dir);
+            string v = textFileReader.ReadToEnd();
+            textFileReader.Close();
+            return v;
+        }
+
+        public string ResetFile(int line, string mes)
+        {
+            string all = ReadAll(); 
+            string[] separator = { Environment.NewLine };
+            string[] allStrings = all.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            StreamWriter textFileWriter = new StreamWriter(dir, false);
+            textFileWriter.Write("");
+            textFileWriter.Close();
+            int i = 0;
+            for (i = 0; i < line - 1; i++)
+            {
+                Write(allStrings[i]);
+            }
+            Write(mes);
+            for (i = line; i < allStrings.Length; i++)
+            {
+                Write(allStrings[i]);
+            }
+            return all;
+        }
+    }
+}
---
+++ TextChange/UserTool.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextChange
+{
+    internal class UserTool
+    {
+        TextRedactor tr;
+
+        public void StartWork()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите путь к файлу");
+                string dir = Console.ReadLine();
+                if (File.Exists(dir))
+                {
+                    tr = new TextRedactor(dir);
+                    break;
+                }
+                else Console.WriteLine("Неверный путь");
+            }
+            Console.WriteLine("Работа с текстовым файлом");
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\t1 - записать строку,\n\t2 - получить строку,\n\t3 - получить весь документ,\n\t4 - перезаписать строку\n");
+                    int choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Write("\nВведите строку: ");
+                            tr.Write(Console.ReadLine());
+                            Console.WriteLine("Строка записана...\n");
+                            break;
+                        case 2:
+                            Console.WriteLine("\nВведите номер строки, которую хотите получить");
+                            int line = int.Parse(Console.ReadLine());
+                            if (line < 5000)
+                                Console.WriteLine("\nВаша строка: " + tr.Read(line) + "\n");
+                            else 
+                                Console.WriteLine("Строка слишком далеко...");
+                            break;
+                        case 3:
+                            Console.WriteLine("\nВаш файл:");
+                            Console.WriteLine(tr.ReadAll() + "\n");
+                            break;
+                        case 4:
+                            Console.WriteLine("Введите номер строки, которую хотите перезаписать");
+                            int line1 = int.Parse(Console.ReadLine());
+                            if (tr.Read(line1) != string.Empty)
+                            {
+                                Console.WriteLine("Введите строку:");
+                                string mes = Console.ReadLine();
+                                tr.ResetFile(line1, mes);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Что-то не так, попробуйте ещё раз");
+                }
+            }
+        }
+    }
+}
